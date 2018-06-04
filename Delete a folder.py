7823467d3bfd26e44f7cff63fb66737df7fe4427
Dr Jeff Grover <drjeffgrover@gmail.com>
2018-06-04 10:54:48 -0400

    how to delete a folder

---
+++ Delete a folder.py
@@ -0,0 +1,8 @@
+#Reference: https://stackoverflow.com/questions/6996603/how-to-delete-a-file-or-folder?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+
+import pandas as pd
+import os
+import sys
+import shutil
+
+shutil.rmtree('flatfile')
